3158d5cbd152d3a99c08132ccdb0375865e5e06e
AnwarBujangHuat <anwarbinbujang@gmail.com>
2022-12-19 17:40:51 +0800

    Initial Commit Master

---
+++ app/navigation/routes.js
@@ -0,0 +1,12 @@
+export const routes = {
+  HOME: 'Home',
+  LOGIN: 'Login',
+  REGISTER: 'Register',
+  RESTAURANT: 'Restaurants',
+  ROULETTE: 'Roulette',
+  PROFILE: 'Profile',
+  BOTTOM: 'Bottom',
+  MENU: 'Menu',
+  RATINGS: 'Ratings',
+  MODAL: 'Modal',
+};
