d026377cfaa9c85b263d2656d523106a913124a5
AnwarBujangHuat <anwarbinbujang@gmail.com>
2022-12-27 11:21:54 +0800

    :bug: change from Math.random() to more secureRandomNumber

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { NavStack } from './app/navigation/NavStack';
+import { StatusBar } from 'react-native';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      {/*<StatusBar barStyle={'dark-content'} />*/}
+      <StatusBar barStyle={'light-content'} />
+      <NavStack />
+    </NavigationContainer>
+  );
+}
