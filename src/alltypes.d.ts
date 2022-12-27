e698a3a9077aac87c18cc9e2dc221636049c9b14
AnwarBujangHuat <anwarbinbujang@gmail.com>
2022-12-27 14:26:35 +0800

    :adhesive_bandage: moving to configs

--- src/alltypes.d.ts
+++ src/alltypes.d.ts
@@ -2,7 +2,10 @@ declare module "*.jpg" {
   const path: string;
   export default path;
 }
-
+declare module "*.png" {
+  const value: import("react-native").ImageSourcePropType;
+  export default value;
+}
 declare module "*.png" {
   const value: any;
   export = value;
